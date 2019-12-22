a14ac91f1b91bb8263e1d8cfbbea73d2f24ed38b
Shweta1319 <47740471+Shweta1319@users.noreply.github.com>
2019-12-22 20:06:49 +0530

    Create Jenkinsfile.sh

---
+++ Jenkinsfile.sh
@@ -0,0 +1,34 @@
+pipeline {
+
+    agent {
+      label "master"
+     }
+          
+    tools {
+       maven 'Maven' 
+       jdk 'Java'      
+    }
+       
+    stage('Build') {
+            steps {
+                sh 'mvn install -DskipTests=true'
+            }
+    }
+        
+     stage('Test'){
+            steps{
+              sh 'mvn test'
+            }
+     }
+     
+     stage('Deploy'){
+            steps{
+              sh 'mvn deploy'
+            }
+     }
+     
+}
+            
+        
+        
+          
